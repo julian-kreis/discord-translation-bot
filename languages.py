3cfc70db804a8d246a93c2ecd3673f0fb3a4edd8
julian-kreis <jakreis@wpi.edu>
2026-07-01 15:27:03 -0700

    added languages dict

--- languages.py
+++ languages.py
@@ -1,5 +1,33 @@
 REGIONAL_BASE = 0x1F1E6
 
+LANGUAGES_DICT = {
+    "Afrikaans": "Afrikaans", "Albanian": "Shqip", "Amharic": "አማርኛ", "Arabic": "العربية",
+    "Armenian": "Հայերեն", "Assamese": "অসমীয়া", "Azerbaijani": "Azərbaycan", "Basque": "Euskara",
+    "Belarusian": "Беларуская", "Bengali": "বাংলা", "Bosnian": "Bosanski", "Bulgarian": "Български",
+    "Burmese": "မြန်မာစာ", "Catalan": "Català", "Cebuano": "Cebuano", "Chinese (Simplified)": "简体中文",
+    "Chinese (Traditional)": "繁體中文", "Corsican": "Corsu", "Croatian": "Hrvatski", "Czech": "Čeština",
+    "Danish": "Dansk", "Dutch": "Nederlands", "English": "English", "Esperanto": "Esperanto",
+    "Estonian": "Eesti", "Filipino": "Filipino", "Finnish": "Suomi", "French": "Français",
+    "Frisian": "Frysk", "Galician": "Galego", "Georgian": "ქართული", "German": "Deutsch",
+    "Greek": "Ελληνικά", "Gujarati": "ગુજરાતી", "Hausa": "Hausa", "Hawaiian": "ʻŌlelo Hawaiʻi",
+    "Hebrew": "עברית", "Hindi": "हिन्दी", "Hmong": "Hmoob", "Hungarian": "Magyar",
+    "Icelandic": "Íslenska", "Igbo": "Igbo", "Indonesian": "Bahasa Indonesia", "Irish": "Gaeilge",
+    "Italian": "Italiano", "Japanese": "日本語", "Javanese": "Basa Jawa", "Kannada": "ಕನ್ನಡ",
+    "Kazakh": "Қазақ тілі", "Khmer": "ភាសាខ្មែរ", "Korean": "한국어", "Kurdish": "Kurdî",
+    "Kyrgyz": "Кыргызча", "Lao": "ລາວ", "Latin": "Latine", "Latvian": "Latviešu",
+    "Lithuanian": "Lietuvių", "Luxembourgish": "Lëtzebuergesch", "Macedonian": "Македонски",
+    "Malay": "Bahasa Melayu", "Malayalam": "മലയാളം", "Maltese": "Malti", "Maori": "Māori",
+    "Marathi": "मराठी", "Mongolian": "Монгол", "Nepali": "नेपाली", "Norwegian": "Norsk",
+    "Persian": "فارسی", "Polish": "Polski", "Portuguese": "Português", "Punjabi": "ਪੰਜਾਬੀ",
+    "Romanian": "Română", "Russian": "Русский", "Samoan": "Gagana Sāmoa",
+    "Serbian": "Srpski", "Shona": "chiShona", "Sindhi": "سنڌي", "Sinhala": "සිංහල",
+    "Slovak": "Slovenčina", "Slovenian": "Slovenščina", "Somali": "Soomaaliga",
+    "Spanish": "Español", "Swahili": "Kiswahili", "Swedish": "Svenska",
+    "Tamil": "தமிழ்", "Telugu": "తెలుగు", "Thai": "ไทย", "Turkish": "Türkçe",
+    "Ukrainian": "Українська", "Urdu": "اردو", "Uzbek": "Oʻzbekcha",
+    "Vietnamese": "Tiếng Việt", "Welsh": "Cymraeg", "Yoruba": "Yorùbá", "Zulu": "isiZulu"
+}
+
 # --- Language Constants ---
 ENGLISH = "English"
 FRENCH = "French"
@@ -31,43 +59,192 @@ ARABIC = "Arabic"
 THAI = "Thai"
 VIETNAMESE = "Vietnamese"
 HINDI = "Hindi"
+AFRIKAANS = "Afrikaans"
+ALBANIAN = "Albanian"
+AMHARIC = "Amharic"
+ARMENIAN = "Armenian"
+ASSAMESE = "Assamese"
+AZERBAIJANI = "Azerbaijani"
+BASQUE = "Basque"
+BELARUSIAN = "Belarusian"
+BENGALI = "Bengali"
+BOSNIAN = "Bosnian"
+BURMESE = "Burmese"
+CATALAN = "Catalan"
+CEBUANO = "Cebuano"
+CORSICAN = "Corsican"
+CROATIAN = "Croatian"
+ESTONIAN = "Estonian"
+ESPERANTO = "Esperanto"
+FILIPINO = "Filipino"
+FRISIAN = "Frisian"
+GALICIAN = "Galician"
+GEORGIAN = "Georgian"
+GUJARATI = "Gujarati"
+HAUSA = "Hausa"
+HAWAIIAN = "Hawaiian"
+HMONG = "Hmong"
+ICELANDIC = "Icelandic"
+INDONESIAN = "Indonesian"
+IRISH = "Irish"
+IGBO = "Igbo"
+JAVANESE = "Javanese"
+KANNADA = "Kannada"
+KAZAKH = "Kazakh"
+KHMER = "Khmer"
+KURDISH = "Kurdish"
+KYRGYZ = "Kyrgyz"
+LATIN = "Latin"
+LATVIAN = "Latvian"
+LAO = "Lao"
+LITHUANIAN = "Lithuanian"
+LUXEMBOURGISH = "Luxembourgish"
+MACEDONIAN = "Macedonian"
+MALAGASY = "Malagasy"
+MALAY = "Malay"
+MALAYALAM = "Malayalam"
+MALTESE = "Maltese"
+MAORI = "Maori"
+MARATHI = "Marathi"
+MONGOLIAN = "Mongolian"
+NEPALI = "Nepali"
+NYANJA = "Nyanja"
+ODIA = "Odia"
+PASHTO = "Pashto"
+PERSIAN = "Persian"
+PUNJABI = "Punjabi"
+SAMOAN = "Samoan"
+SCOTTISH_GAELIC = "Scottish Gaelic"
+SERBIAN = "Serbian"
+SHONA = "Shona"
+SINDHI = "Sindhi"
+SINHALA = "Sinhala"
+SLOVENIAN = "Slovenian"
+SOMALI = "Somali"
+SOTHO = "Sotho"
+SUNDANESE = "Sundanese"
+SWAHILI = "Swahili"
+TAJIK = "Tajik"
+TAMIL = "Tamil"
+TELUGU = "Telugu"
+URDU = "Urdu"
+UZBEK = "Uzbek"
+WELSH = "Welsh"
+XHOSA = "Xhosa"
+YIDDISH = "Yiddish"
+YORUBA = "Yoruba"
+ZULU = "Zulu"
 
 # --- Country to Language Mapping ---
 COUNTRY_LANGUAGE = {
-    "US": ENGLISH, "GB": ENGLISH, "AU": ENGLISH,
-    "CA": ENGLISH, "NZ": ENGLISH, "IE": ENGLISH,
-    "FR": FRENCH, "BE": FRENCH, "CH": FRENCH,
-    "ES": SPANISH, "MX": SPANISH, "AR": SPANISH,
-    "CL": SPANISH, "CO": SPANISH, "PE": SPANISH,
-    "PT": PORTUGUESE, "BR": PORTUGUESE,
-    "DE": GERMAN, "AT": GERMAN,
-    "CN": SIMPLIFIED_CHINESE,
-    "SG": SIMPLIFIED_CHINESE,
-    "TW": TRADITIONAL_CHINESE,
-    "HK": TRADITIONAL_CHINESE,
+    # --- Existing Languages with Expanded Flags ---
+    "US": ENGLISH, "GB": ENGLISH, "AU": ENGLISH, "CA": ENGLISH, "NZ": ENGLISH, "IE": ENGLISH, "ZA": ENGLISH, "SG": ENGLISH,
+    "FR": FRENCH, "BE": FRENCH, "CH": FRENCH, "CA": FRENCH, "LU": FRENCH, "MC": FRENCH,
+    "ES": SPANISH, "MX": SPANISH, "AR": SPANISH, "CL": SPANISH, "CO": SPANISH, "PE": SPANISH, "VE": SPANISH, "CU": SPANISH, "DO": SPANISH,
+    "PT": PORTUGUESE, "BR": PORTUGUESE, "AO": PORTUGUESE, "MZ": PORTUGUESE,
+    "DE": GERMAN, "AT": GERMAN, "CH": GERMAN, "LU": GERMAN, "LI": GERMAN,
+    "CN": SIMPLIFIED_CHINESE, "SG": SIMPLIFIED_CHINESE, "MY": SIMPLIFIED_CHINESE,
+    "TW": TRADITIONAL_CHINESE, "HK": TRADITIONAL_CHINESE, "MO": TRADITIONAL_CHINESE,
     "JP": JAPANESE,
-    "KR": KOREAN,
-    "IT": ITALIAN,
-    "NL": DUTCH,
-    "SE": SWEDISH,
+    "KR": KOREAN, "KP": KOREAN,
+    "IT": ITALIAN, "CH": ITALIAN, "SM": ITALIAN,
+    "NL": DUTCH, "BE": DUTCH, "SR": DUTCH,
+    "SE": SWEDISH, "FI": SWEDISH,
     "NO": NORWEGIAN,
-    "DK": DANISH,
+    "DK": DANISH, "GL": DANISH,
     "FI": FINNISH,
     "PL": POLISH,
     "CZ": CZECH,
     "SK": SLOVAK,
-    "HU": HUNGARIAN,
-    "RO": ROMANIAN,
+    "HU": HUNGARIAN, "RO": HUNGARIAN,
+    "RO": ROMANIAN, "MD": ROMANIAN,
     "BG": BULGARIAN,
-    "GR": GREEK,
-    "TR": TURKISH,
-    "RU": RUSSIAN,
+    "GR": GREEK, "CY": GREEK,
+    "TR": TURKISH, "CY": TURKISH,
+    "RU": RUSSIAN, "BY": RUSSIAN, "KZ": RUSSIAN, "KG": RUSSIAN,
     "UA": UKRAINIAN,
     "IL": HEBREW,
-    "SA": ARABIC, "AE": ARABIC, "EG": ARABIC,
+    "SA": ARABIC, "AE": ARABIC, "EG": ARABIC, "MA": ARABIC, "DZ": ARABIC, "TN": ARABIC, "IQ": ARABIC, "JO": ARABIC, "LB": ARABIC,
     "TH": THAI,
     "VN": VIETNAMESE,
-    "IN": HINDI,
+    "IN": HINDI, "FJ": HINDI,
+
+    # --- Additional Languages with Multiple Flags ---
+    "ZA": AFRIKAANS, "NA": AFRIKAANS,
+    "AL": ALBANIAN, "XK": ALBANIAN, "MK": ALBANIAN,
+    "ET": AMHARIC,
+    "AM": ARMENIAN,
+    "IN_AS": ASSAMESE,
+    "AZ": AZERBAIJANI, "IR": AZERBAIJANI,
+    "ES_PV": BASQUE,
+    "BY": BELARUSIAN,
+    "BD": BENGALI, "IN": BENGALI,
+    "BA": BOSNIAN,
+    "MM": BURMESE,
+    "AD": CATALAN, "ES": CATALAN, "FR": CATALAN,
+    "PH_CEB": CEBUANO,
+    "FR_COR": CORSICAN,
+    "HR": CROATIAN, "BA": CROATIAN,
+    "EE": ESTONIAN,
+    "EO": ESPERANTO,  # International/Constructed
+    "PH": FILIPINO,
+    "NL_FR": FRISIAN, "DE": FRISIAN,
+    "ES_GA": GALICIAN,
+    "GE": GEORGIAN,
+    "IN_GJ": GUJARATI,
+    "NG": HAUSA, "NE": HAUSA, "GH": HAUSA,
+    "US_HI": HAWAIIAN,
+    "CN_HU": HMONG, "VN": HMONG, "LA": HMONG, "TH": HMONG,
+    "IS": ICELANDIC,
+    "ID": INDONESIAN, "TL": INDONESIAN,
+    "IE_GA": IRISH, "GB": IRISH,
+    "NG_IG": IGBO,
+    "ID_JV": JAVANESE, "SR": JAVANESE, "MY": JAVANESE,
+    "IN_KA": KANNADA,
+    "KZ": KAZAKH, "CN": KAZAKH, "MN": KAZAKH,
+    "KH": KHMER, "VN": KHMER,
+    "IQ_KU": KURDISH, "TR": KURDISH, "IR": KURDISH, "SY": KURDISH,
+    "KG": KYRGYZ,
+    "VA": LATIN,
+    "LV": LATVIAN,
+    "LA": LAO,
+    "LT": LITHUANIAN,
+    "LU": LUXEMBOURGISH,
+    "MK": MACEDONIAN,
+    "MG": MALAGASY,
+    "MY": MALAY, "SG": MALAY, "BN": MALAY, "ID": MALAY,
+    "IN_KL": MALAYALAM,
+    "MT": MALTESE,
+    "NZ_MI": MAORI,
+    "IN_MH": MARATHI,
+    "MN": MONGOLIAN, "CN": MONGOLIAN,
+    "NP": NEPALI, "IN": NEPALI,
+    "MW": NYANJA, "ZM": NYANJA, "MZ": NYANJA,
+    "IN_OR": ODIA,
+    "AF": PASHTO, "PK": PASHTO,
+    "IR": PERSIAN, "AF": PERSIAN, "TJ": PERSIAN,
+    "IN_PB": PUNJABI, "PK": PUNJABI,
+    "WS": SAMOAN, "AS": SAMOAN,
+    "GB_SCT": SCOTTISH_GAELIC,
+    "RS": SERBIAN, "BA": SERBIAN, "ME": SERBIAN,
+    "ZW": SHONA, "MZ": SHONA,
+    "PK_SD": SINDHI, "IN": SINDHI,
+    "LK": SINHALA,
+    "SI": SLOVENIAN,
+    "SO": SOMALI, "ET": SOMALI, "DJ": SOMALI, "KE": SOMALI,
+    "LS": SOTHO, "ZA": SOTHO,
+    "ID_SU": SUNDANESE,
+    "TZ": SWAHILI, "KE": SWAHILI, "UG": SWAHILI, "CD": SWAHILI, "RW": SWAHILI,
+    "TJ": TAJIK, "UZ": TAJIK,
+    "IN_TN": TAMIL, "LK": TAMIL, "SG": TAMIL, "MY": TAMIL,
+    "IN_AP": TELUGU,
+    "PK": URDU, "IN": URDU,
+    "UZ": UZBEK, "AF": UZBEK, "TJ": UZBEK,
+    "GB_CYM": WELSH,
+    "ZA_XH": XHOSA,
+    "IL_YI": YIDDISH, "US": YIDDISH,
+    "NG_YO": YORUBA, "BJ": YORUBA, "TG": YORUBA,
+    "ZA_ZU": ZULU,
 }
 
 def flag_to_country(flag):
